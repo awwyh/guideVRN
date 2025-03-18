8c126b8847991b9fdec3c2721ed5f6b230ae74de
rokyrou <93413198+awwyh@users.noreply.github.com>
2025-03-18 21:38:37 +0300

    Add files via upload

---
+++ GuideVRN/js/index.js
@@ -0,0 +1,61 @@
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+                if (!animItem.classList.contains('_active')) {
+                    animItem.classList.add('_active');
+                    animateNumbers(animItem);
+                }
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+            scrollTop = window.scrollY || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
+
+function sm_menu() {
+    document.getElementById('top_menu_check').checked = false;
+}
+
+
+
+$(document).ready(function() {
+
+   
+    $("a.scroll-to").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        document.getElementById('top_menu_check').checked = false;
+        return false;
+    });
+});
